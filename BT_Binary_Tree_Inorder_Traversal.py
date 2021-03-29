fd43ef54ba484f5bf45989c5931e37f46b45beb1
PY Hsu <daipeinew.dif02@g2.nctu.edu.tw>
2021-03-30 02:23:20 +0800

    Add binary tree inorder traversal

---
+++ BT_Binary_Tree_Inorder_Traversal.py
@@ -0,0 +1,44 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ans_list=[]
+        def traverse(cur_root):
+            if cur_root:
+                traverse(cur_root.left)
+                ans_list.append(cur_root.val)
+                traverse(cur_root.right)
+            else:
+                return
+            return
+        if root:
+            cur_root = root
+            traverse(cur_root)
+        else:
+            return []
+        return ans_list
+#%% Iterative
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        ans = []
+        stack = []
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                tmpNode = stack.pop()
+                ans.append(tmpNode.val)
+                root = tmpNode.right
+
+        return ans
