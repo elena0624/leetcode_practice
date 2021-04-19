285bc922dbab22516212accbb07989c09da54aa6
PY Hsu <daipeinew.dif02@g2.nctu.edu.tw>
2021-04-19 23:54:07 +0800

    Save number of submatrices that sum to target1

--- Challenge_Number_of_Submatrices_That_Sum_to_Target1.py
+++ Challenge_Number_of_Submatrices_That_Sum_to_Target1.py
@@ -30,24 +30,43 @@ for i in range(m):
         
         
 print(rec)
-        
+# 以上是計算所有右下角的和，不過不知道怎麼用        
 #%%
 import collections
-matrix = [[0,1,0],[1,1,1],[0,1,0],[0,1,0]]
+#matrix = [[0,1,0],[1,1,1],[0,1,0],[0,1,0]]
+matrix = [[0,1,0],[1,1,1],[0,1,0]]
+target=0
 
-A=matrix
-m, n = len(A), len(A[0])
-for row in A:
-    for i in range(n - 1):
-        row[i + 1] += row[i]
+matrix = [[1,-1],[-1,1]]
+target = 0
 
-res = 0
+
+m, n = len(matrix), len(matrix[0])
+#print('m',m)
+row_sum = [[0]*(n+1) for i in range(m)]
+print(row_sum)
+# compute row presum
+for i in range(m):
+    row_sum[i][1]=matrix[i][0]
+    for j in range(2,n+1):
+#        print(i,j)
+        row_sum[i][j]=matrix[i][j-1]+row_sum[i][j-1]
+print(row_sum)
+
+ans=0
+# select each 2 columns and turns to problem 560
 for i in range(n):
-    for j in range(i, n):
-        c = collections.defaultdict(int)
-        cur, c[0] = 0, 1
+    for j in range(i+1,n+1):
+        print(i,j)
+        sub_matrix_sum = collections.defaultdict(int)
+        sub_matrix_sum[0]=1
+        cur_sum=0
         for k in range(m):
-            cur += A[k][j] - (A[k][i - 1] if i > 0 else 0)
-            res += c[cur - target]
-            c[cur] += 1
-print(res)
+            cur_sum+=(row_sum[k][j]-row_sum[k][i])
+            print(cur_sum)
+            ans+= sub_matrix_sum[cur_sum-target]
+            sub_matrix_sum[cur_sum]+=1
+            print('ans',ans)
+        print(sub_matrix_sum)
+        print(ans)
+print(ans)
