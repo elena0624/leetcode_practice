7c7f12ff2e38472fc5caccdf0e36aec2b2e8e575
PY Hsu <daipeinew.dif02@g2.nctu.edu.tw>
2021-05-19 18:36:42 +0800

    Longest string chain1 update

--- Challenge_Longest_String_Chain1.py
+++ Challenge_Longest_String_Chain1.py
@@ -49,3 +49,54 @@ for i in range(len(words)):
     
 #print(memo)
 print(ans)
+#%%
+import collections
+
+words = ["a","b","ba","bca","bda","bdca"]
+words = ["xbc","pcxbcf","xb","cxbc","pcxbc"]
+
+words = sorted(words, key=lambda x:len(x), reverse=True)
+
+#memo = [0 for i in range(len(words))]
+memo = collections.defaultdict(int)
+
+def check_pre(cur_str):
+    if memo[cur_str]:
+        return memo[cur_str]
+    remove_chr = [cur_str[:k-1]+cur_str[k:] for k in range(1,len(cur_str))]+[cur_str[:-1]]
+    max_step=1
+    for i in range(len(words)):
+        if words[i] in remove_chr:
+            cur_step = check_pre(words[i])+1
+            max_step=max(max_step,cur_step)
+    memo[cur_str]=max_step
+    return max_step
+
+ans= 0
+for i in range(len(words)):
+    check_pre(words[i])
+    ans=max(memo[words[i]],ans)
+    
+print(ans)
+#%%
+import collections
+
+#words = ["a","b","ba","bca","bda","bdca"]
+words = ["xbc","pcxbcf","xb","cxbc","pcxbc"]
+
+words = sorted(words, key=lambda x:len(x))
+
+memo = collections.defaultdict(int)
+
+ans=0
+for i in range(len(words)):
+    cur_longest=1
+#    print(words[i])
+    remove_chr = [words[i][:k-1]+words[i][k:] for k in range(1,len(words[i]))]+[words[i][:-1]]
+#    print(remove_chr)
+    for j in remove_chr:
+        if memo[j]:
+            cur_longest=max(cur_longest,memo[j]+1)
+    memo[words[i]]=cur_longest
+    ans=max(ans,cur_longest)
+print(ans)
